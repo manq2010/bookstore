209422ac4b74b129c0e9062916ae14e85251c83c
manq2010 <manq2010@gmail.com>
2022-12-15 11:16:36 +0200

    Add styling to footer

--- src/features/Logo/Logo.jsx
+++ src/features/Logo/Logo.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const LogoContainer = styled.h1`
+  display: none;
+  font-size: 2rem;
+  font-family: "Montserrat",sans-serif;
+  font-size: 1.875rem;
+  font-weight: 700;
+  color: #0290ff;
+
+@media (min-width: 768px) {
+    display: block;
+}
+`;
 
 const Logo = () => {
   const navigate = useNavigate();
   return (
     <>
-      <h1 className="logo" onClick={() => navigate('/')} aria-hidden="true">
+      <LogoContainer className="logo" onClick={() => navigate('/')} aria-hidden="true">
         Bookstore CMS
-      </h1>
+      </LogoContainer>
     </>
   );
 };
